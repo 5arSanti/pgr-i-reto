e9d6c6766c6d9ffd648a644617928dc1f107b357
5arSanti <santiari05@hotmail.com>
2025-01-24 20:59:48 -0500

    Layout, styles: Task cards

--- frontend/src/Pages/Components/TasksContainer/index.jsx
+++ frontend/src/Pages/Components/TasksContainer/index.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { TaskCard } from "../TaskCard";
 import "./styles.css";
 
 const TasksContainer = ({subtitle, className}) => {
@@ -6,7 +7,18 @@ const TasksContainer = ({subtitle, className}) => {
 		<div className="main-tasks-container">
 			<p className="task-subtitle">{subtitle}</p>
 			<div className={`${className} tasks-wrapper`}>
-
+				<TaskCard
+					task={{Nombre: "Tarea 1", Descripcion: "Descripcion de la tarea 1"}}
+					className={className}
+				/>
+				<TaskCard
+					task={{Nombre: "Tarea 2", Descripcion: "Descripcion de la tarea 2"}}
+					className={className}
+				/>
+				<TaskCard
+					task={{Nombre: "Tarea 3", Descripcion: "Descripcion de la tarea 3"}}
+					className={className}
+				/>
 			</div>
 		</div>
 	)
